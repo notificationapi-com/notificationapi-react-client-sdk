a04d982283741139ae4c61911cdc3805d55cb461
Sahand Seifi <sahand.seifi@gmail.com>
2024-05-28 14:31:58 -0400

    Improve build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
@@ -16,9 +18,25 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync("lib/**/*.{ts,tsx}", {
+            ignore: ["lib/**/*.d.ts"],
+          })
+          .map((file) => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
       output: {
-        assetFileNames: "assets/[name][extname]",
         entryFileNames: "[name].js",
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: "assets/[name].[ext]",
       },
     },
   },
