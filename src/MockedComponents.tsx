8d21fe9324016da66d37a0c23c9234db0fbf2b3b
mbasadi <asadi.mohammadbagher@gmail.com>
2024-10-18 11:15:24 -0400

    Refactor App component to add live and mocked mode switch

---
+++ src/MockedComponents.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import {
+  NotificationFeed,
+  NotificationPopup,
+  NotificationLauncher,
+  NotificationCounter,
+  NotificationAPIProvider,
+  NotificationPreferencesPopup,
+  NotificationPreferencesInline
+} from '../lib/main';
+import { Divider, Button } from 'antd';
+import { MyButton } from './MyButton';
+import { FakeNotification } from './FakeNotification';
+import { mockedClient } from './mockedClient';
+
+const LiveComponents: React.FC = () => {
+  const [preferencesPopupVisibility, setPreferencesPopupVisiblity] =
+    useState(false);
+
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>Mocked:</h1>
+      <h2 style={{ textAlign: 'center' }}>
+        Experience the magic of notifications!
+      </h2>
+      <div
+        style={{
+          height: '210vh',
+          background: '#f0f2f5',
+          padding: 24
+        }}
+      >
+        <NotificationAPIProvider
+          clientId="24nojpnrsdc53fkslha0roov05"
+          user={{
+            id: 'mockedUser',
+            email: 'mockedUser@gmail.com'
+          }}
+          client={mockedClient}
+        >
+          <h3 style={{ textAlign: 'center' }}>
+            Fill out the form below to generate a fake notification and see it
+            in action!
+          </h3>
+          <FakeNotification />
+          <Divider />
+          <h2>Popup:</h2>
+          <NotificationPopup
+            count={(n) => {
+              return !n.archived;
+            }}
+          />
+
+          <Divider />
+
+          <h2>Launcher:</h2>
+          <p>Look at the bottom right :)</p>
+          <NotificationLauncher
+            count={(n) => {
+              return !n.archived;
+            }}
+          />
+
+          <Divider />
+
+          <h2>Counter (Standalone)</h2>
+          <NotificationCounter
+            count={(n) => {
+              return !n.archived;
+            }}
+          />
+
+          <Divider />
+
+          <h2>Counter on an element</h2>
+          <NotificationCounter
+            count={(n) => {
+              return !n.archived;
+            }}
+          >
+            <MyButton />
+          </NotificationCounter>
+          <Divider />
+          <h2>Feed:</h2>
+          <NotificationFeed infiniteScrollHeight={300} />
+
+          <Divider />
+          <h2>Preferences Popup:</h2>
+          <Button onClick={() => setPreferencesPopupVisiblity(true)}>
+            Preferences Popup
+          </Button>
+          <NotificationPreferencesPopup
+            open={preferencesPopupVisibility}
+            onClose={() => {
+              setPreferencesPopupVisiblity(false);
+            }}
+          />
+
+          <h2>Preferences Inline:</h2>
+          <NotificationPreferencesInline />
+        </NotificationAPIProvider>
+      </div>
+    </>
+  );
+};
+
+export default LiveComponents;
