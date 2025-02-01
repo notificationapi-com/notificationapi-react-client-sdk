91926f3f1df78000795cccac5948c2c0a2594488
millerm30 <millergm30@gmail.com>
2025-01-31 20:40:17 -0330

    Bundle css with js with move to mui to fix compile for customers

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,44 @@
-import { defineConfig } from "vite";
-import { extname, relative, resolve } from "path";
-import { fileURLToPath } from "node:url";
-import { glob } from "glob";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
+  plugins: [react(), dts({ include: ['lib'] })],
   build: {
+    cssCodeSplit: false,
+    cssMinify: true,
     emptyOutDir: true,
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
+      external: ['react', 'react/jsx-runtime'],
       input: Object.fromEntries(
         // https://rollupjs.org/configuration-options/#input
         glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: ["lib/**/*.d.ts"],
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: ['lib/**/*.d.ts']
           })
           .map((file) => [
             // 1. The name of the entry point
             // lib/nested/foo.js becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
+            relative('lib', file.slice(0, file.length - extname(file).length)),
             // 2. The absolute path to the entry file
             // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
+            fileURLToPath(new URL(file, import.meta.url))
           ])
       ),
       output: {
-        entryFileNames: "[name].js",
+        entryFileNames: '[name].js',
         chunkFileNames: `assets/[name].js`,
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-  },
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 });
